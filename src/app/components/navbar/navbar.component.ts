2cebb07f8c29eab946d83785a338472e65b49372
matheorose <matheorose1703@gmail.com>
2025-11-11 20:45:57 +0100

    update: nav upgrade

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,14 +8,41 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy {
   menuOpen = false;
+  hideNavButtons = false;
+  private lastScrollTop = 0;
+  private readonly scrollThreshold = 20;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+    this.setBodyScroll(!this.menuOpen);
   }
 
   closeMenu() {
     this.menuOpen = false;
+    this.setBodyScroll(true);
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop || 0;
+    const isScrollingDown = currentScroll > this.lastScrollTop;
+
+    if (isScrollingDown && currentScroll > this.scrollThreshold) {
+      this.hideNavButtons = true;
+    } else {
+      this.hideNavButtons = false;
+    }
+
+    this.lastScrollTop = Math.max(currentScroll, 0);
+  }
+
+  ngOnDestroy() {
+    this.setBodyScroll(true);
+  }
+
+  private setBodyScroll(enable: boolean) {
+    document.body.style.overflow = enable ? '' : 'hidden';
   }
 }
