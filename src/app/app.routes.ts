3fec226f723cf0e791ad24c5197424b02cb10264
SemihAslan123 <semihaslan2210@gmail.com>
2025-11-11 21:47:30 +0100

    commit logo et routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { ServicesComponent } from './views/services/services.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
 ];
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  imports: [RouterLink],
 })
 export class HomeComponent implements OnInit {
   ngOnInit(): void {
