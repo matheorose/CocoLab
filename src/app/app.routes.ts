82ff103360a22957b0b9a42b198d954596d4a0bc
matheorose <matheorose1703@gmail.com>
2025-11-23 01:04:51 +0100

    update: mentions legales template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './views/home/home.component';
 import { ContactComponent } from './views/contact/contact.component';
 import { ServicesComponent } from './views/services/services.component';
 import { AboutComponent } from './views/about/about.component';
+import { LegalComponent } from './views/legal/legal.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'services', component: ServicesComponent },
+  { path: 'mentions-legales', component: LegalComponent },
 ];
---
+++ src/app/views/legal/legal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-legal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './legal.component.html',
+  styleUrl: './legal.component.css'
+})
+export class LegalComponent {}
